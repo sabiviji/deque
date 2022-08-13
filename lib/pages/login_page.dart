b32d813d7976d0a189e005d5ca3dcae35aed84bb
Sabi Viji <sabipalathinkal10@gmail.com>
2022-08-13 15:15:15 +0530

    Login page added with firebase support

---
+++ lib/pages/login_page.dart
@@ -0,0 +1,147 @@
+import 'package:deque/pages/home_page.dart';
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:flutter/material.dart';
+import 'package:fluttertoast/fluttertoast.dart';
+
+class LoginPage extends StatefulWidget {
+  const LoginPage({Key? key}) : super(key: key);
+
+  @override
+  _LoginPageState createState() => _LoginPageState();
+}
+
+class _LoginPageState extends State<LoginPage> {
+  //form key
+  final _formKey = GlobalKey<FormState>();
+
+  // editing controller
+  final TextEditingController emailController = TextEditingController();
+  final TextEditingController passwordController = TextEditingController();
+
+  // firebase
+  final _auth = FirebaseAuth.instance;
+
+  @override
+  Widget build(BuildContext context) {
+    // email field
+    final emailField = TextFormField(
+      autofocus: false,
+      controller: emailController,
+      keyboardType: TextInputType.emailAddress,
+      validator: ((value) {
+        if (value!.isEmpty) {
+          return ("Please enter your email");
+        }
+        // regluar expression for email validation
+        if (!RegExp("^[a-zA-Z0-9+_.-]+@[a-zA-Z0-9.-]+.[a-z]").hasMatch(value)) {
+          return ("Please enter a valid email");
+        }
+        return null;
+      }),
+      onSaved: (newValue) {
+        emailController.text = newValue!;
+      },
+      textInputAction: TextInputAction.next,
+      decoration: InputDecoration(
+          prefixIcon: Icon(Icons.mail),
+          contentPadding: EdgeInsets.fromLTRB(10, 15, 20, 15),
+          hintText: 'Email',
+          border: OutlineInputBorder(borderRadius: BorderRadius.circular(20))),
+    );
+
+    // password field
+    final passwordField = TextFormField(
+      autofocus: false,
+      controller: passwordController,
+      obscureText: true,
+      validator: ((value) {
+        RegExp regex = new RegExp(r'^.{6,}$');
+        if (value!.isEmpty) {
+          return ("Password required");
+        }
+        if (!regex.hasMatch(value)) {
+          return ("Enter valid password (Min. 6 characters)");
+        }
+        return null;
+      }),
+      onSaved: (newValue) {
+        passwordController.text = newValue!;
+      },
+      textInputAction: TextInputAction.done,
+      decoration: InputDecoration(
+          prefixIcon: Icon(Icons.vpn_key),
+          contentPadding: EdgeInsets.fromLTRB(10, 15, 20, 15),
+          hintText: 'Password',
+          border: OutlineInputBorder(borderRadius: BorderRadius.circular(20))),
+    );
+
+    // login button
+    final loginButton = Material(
+      elevation: 5,
+      borderRadius: BorderRadius.circular(30),
+      color: Colors.deepPurpleAccent,
+      child: MaterialButton(
+        padding: EdgeInsets.fromLTRB(10, 15, 20, 15),
+        minWidth: MediaQuery.of(context).size.width,
+        onPressed: (() {
+          signIn(emailController.text, passwordController.text);
+        }),
+        child: Text(
+          'Login',
+          textAlign: TextAlign.center,
+          style: TextStyle(
+              fontSize: 20, color: Colors.white, fontWeight: FontWeight.bold),
+        ),
+      ),
+    );
+
+    return Scaffold(
+        backgroundColor: Colors.white,
+        body: Center(
+          child: SingleChildScrollView(
+              child: Container(
+            color: Colors.white,
+            padding: const EdgeInsets.all(36),
+            child: Form(
+              key: _formKey,
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                crossAxisAlignment: CrossAxisAlignment.center,
+                children: <Widget>[
+                  SizedBox(
+                    height: 200,
+                    child: Image.asset(
+                      'assets/images/logo.svg',
+                      fit: BoxFit.contain,
+                    ),
+                  ),
+                  SizedBox(height: 45),
+                  emailField,
+                  SizedBox(height: 25),
+                  passwordField,
+                  SizedBox(height: 35),
+                  loginButton,
+                  SizedBox(height: 15)
+                ],
+              ),
+            ),
+          )),
+        ));
+  }
+
+  // login function
+  void signIn(String email, String password) async {
+    if (_formKey.currentState!.validate()) {
+      await _auth
+          .signInWithEmailAndPassword(email: email, password: password)
+          .then((uid) => {
+                Fluttertoast.showToast(msg: "Login successful!"),
+                Navigator.of(context).pushReplacement(
+                    MaterialPageRoute(builder: ((context) => HomePage())))
+              })
+          .catchError((e) {
+        Fluttertoast.showToast(msg: e!.message);
+      });
+    }
+  }
+}
